51af92dca7becac9acb7bb8dc2538be900b66d3a
Leon8M <106140292+Leon8M@users.noreply.github.com>
2025-11-12 10:15:29 +0300

    first commit

---
+++ src/main/java/com/cardi/cardi/CardiApplication.java
@@ -0,0 +1,13 @@
+package com.cardi.cardi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardi/cardi/CardiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardi.cardi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
